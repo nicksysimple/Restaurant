5fe97857165f2f0ce1111472810cfc090752ca38
nicksysimple <kipkorirnickson45@gmail.com>
2020-11-30 11:50:25 +0300

    Added a jumbotrron foe each hotel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,9 @@
  <div class="container">
  	<div class="row">
  		@foreach($resto as $key)
-		 	<div class="col-sm-12 col-md-6 col-lg-6">
+
+    <div class="jumbotron">
+      <div class="col-sm-12 col-md-6 col-lg-6">
 		 		<img class="image col-4" src="images/{{$key->image}}" alt="Hotel Image">
 		 	</div>
 		 	<div class="col-sm-12 col-md-6 col-lg-6"><h2> {{$key->name}}</h2>
@@ -17,6 +19,8 @@
 
 			 	<button id="comment" type="onclick" class="btn btn-primary">Add comment</button>
  			</div>
+    </div>
+
 
 		@endforeach
 
